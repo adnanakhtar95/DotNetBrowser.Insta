30264f210018247ac33dc511c7356e148fd43485
Adnan Akhtar <adnanakhtar95@gmail.com>
2023-07-13 15:12:35 +0500

    keyboard Simulation

--- InstaBrowser/Form1.cs
+++ InstaBrowser/Form1.cs
@@ -102,6 +102,7 @@ namespace InstaBrowser
                     // Assign the content to the Password element
                     password = content;
                 }
+               
                 //ChainInputEventToElement(document.GetElementByName("username"), user_name, "change");
                 //ChainInputEventToElement(document.GetElementByName("password"), password, "change");
 
@@ -165,82 +166,161 @@ namespace InstaBrowser
                 Thread.Sleep(500);
 
                 oElement.Focus();
-
+                KeyModifiers modifiers = null;
                 IElement focusedElement = e.Browser.MainFrame.Document.FocusedElement;
 
 
                 oElement.Focus();
 
-
+               
                 IKeyboard keyboard = e.Browser.Keyboard;
                 KeyCode key;
                 string keyChar;
 
-                KeyPressedEventArgs keyDownEventArgs;
-                KeyTypedEventArgs keyPressEventArgs;
-                KeyReleasedEventArgs keyUpEventArgs;
-
                 for (int iChar = 0, loopTo = value.Length - 1; iChar <= loopTo; iChar++)
                 {
-
-                    //key = (KeyCode)System.Convert.ToInt32(value[iChar]);
-                    key = (KeyCode)(value[iChar]);
+                 
+                    key = (KeyCode)System.Convert.ToInt32(value[iChar]); 
                     keyChar = value[iChar].ToString();
 
-                    //DebugWriteLine("Sending..." + keyChar);
-
-                    keyDownEventArgs = new KeyPressedEventArgs
+                    if (IsSpecialCharacter(Convert.ToChar(keyChar)) && keyChar =="#") 
+                    { 
+                       SimulateKey(keyboard, KeyCode.Oem3, keyChar, new KeyModifiers { ShiftDown = true });
+                    } 
+                    else if (IsSpecialCharacter(Convert.ToChar(keyChar)) && keyChar =="$") 
+                    { 
+                       SimulateKey(keyboard, KeyCode.Oem4, keyChar, new KeyModifiers { ShiftDown = true });
+                    }
+                    else
                     {
-                        KeyChar = keyChar,
-                        VirtualKey = key
-                    };
+                        SimulateKey(keyboard, key, keyChar);
+                    }
 
-                    keyPressEventArgs = new KeyTypedEventArgs
-                    {
-                        KeyChar = keyChar,
-                        VirtualKey = key
-                    };
-                    keyUpEventArgs = new KeyReleasedEventArgs
-                    {
-                        VirtualKey = key
-                    };
-
-
-                    keyboard.KeyPressed.Raise(keyDownEventArgs);
-                    keyboard.KeyTyped.Raise(keyPressEventArgs);
-                    keyboard.KeyReleased.Raise(keyUpEventArgs);
 
                     System.Threading.Thread.Sleep(50);
                 }
 
-                key = KeyCode.Tab;
-                keyChar = "";
-
-                keyDownEventArgs = new KeyPressedEventArgs
-                {
-                    KeyChar = keyChar,
-                    VirtualKey = key
-                };
-
-                keyPressEventArgs = new KeyTypedEventArgs
-                {
-                    KeyChar = keyChar,
-                    VirtualKey = key
-                };
-                keyUpEventArgs = new KeyReleasedEventArgs
-                {
-                    VirtualKey = key
-                };
-
-                keyboard.KeyPressed.Raise(keyDownEventArgs);
-                keyboard.KeyTyped.Raise(keyPressEventArgs);
-                keyboard.KeyReleased.Raise(keyUpEventArgs);
-
+              
             }
             catch (Exception ex)
             {
                 //oLog.WriteErrorLog(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
             }
         }
+        private bool IsSpecialCharacter(Char keyChar)
+        {
+            return Char.IsSymbol(keyChar) || Char.IsPunctuation(keyChar);
+        }
+        private static void SimulateKey(IKeyboard keyboard, KeyCode key, string keyChar,
+                                       KeyModifiers modifiers = null)
+        {
+            modifiers = modifiers ?? new KeyModifiers();
+            KeyPressedEventArgs keyDownEventArgs = new KeyPressedEventArgs
+            {
+                KeyChar = keyChar,
+                VirtualKey = key,
+                Modifiers = modifiers
+            };
+
+            KeyTypedEventArgs keyPressEventArgs = new KeyTypedEventArgs
+            {
+                KeyChar = keyChar,
+                VirtualKey = key,
+                Modifiers = modifiers
+            };
+            KeyReleasedEventArgs keyUpEventArgs = new KeyReleasedEventArgs
+            {
+                VirtualKey = key,
+                Modifiers = modifiers
+            };
+
+            keyboard.KeyPressed.Raise(keyDownEventArgs);
+            keyboard.KeyTyped.Raise(keyPressEventArgs);
+            keyboard.KeyReleased.Raise(keyUpEventArgs);
+        }
+        //private void EnterValue(IInputElement oElement, string value, FrameLoadFinishedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        Thread.Sleep(500);
+
+        //        oElement.Focus();
+
+        //        IElement focusedElement = e.Browser.MainFrame.Document.FocusedElement;
+
+
+        //        oElement.Focus();
+
+
+        //        IKeyboard keyboard = e.Browser.Keyboard;
+        //        KeyCode key;
+        //        string keyChar;
+
+        //        KeyPressedEventArgs keyDownEventArgs;
+        //        KeyTypedEventArgs keyPressEventArgs;
+        //        KeyReleasedEventArgs keyUpEventArgs;
+
+        //        for (int iChar = 0, loopTo = value.Length - 1; iChar <= loopTo; iChar++)
+        //        {
+
+        //            //key = (KeyCode)System.Convert.ToInt32(value[iChar]);
+        //            key = (KeyCode)(value[iChar]);
+        //            keyChar = value[iChar].ToString();
+
+        //            //DebugWriteLine("Sending..." + keyChar);
+
+        //            keyDownEventArgs = new KeyPressedEventArgs
+        //            {
+        //                KeyChar = keyChar,
+        //                VirtualKey = key
+        //            };
+
+        //            keyPressEventArgs = new KeyTypedEventArgs
+        //            {
+        //                KeyChar = keyChar,
+        //                VirtualKey = key
+        //            };
+        //            keyUpEventArgs = new KeyReleasedEventArgs
+        //            {
+        //                VirtualKey = key
+        //            };
+
+
+        //            keyboard.KeyPressed.Raise(keyDownEventArgs);
+        //            keyboard.KeyTyped.Raise(keyPressEventArgs);
+        //            keyboard.KeyReleased.Raise(keyUpEventArgs);
+
+        //            System.Threading.Thread.Sleep(50);
+        //        }
+
+        //        key = KeyCode.Tab;
+        //        keyChar = "";
+
+        //        keyDownEventArgs = new KeyPressedEventArgs
+        //        {
+        //            KeyChar = keyChar,
+        //            VirtualKey = key
+        //        };
+
+        //        keyPressEventArgs = new KeyTypedEventArgs
+        //        {
+        //            KeyChar = keyChar,
+        //            VirtualKey = key
+        //        };
+        //        keyUpEventArgs = new KeyReleasedEventArgs
+        //        {
+        //            VirtualKey = key
+        //        };
+
+        //        keyboard.KeyPressed.Raise(keyDownEventArgs);
+        //        keyboard.KeyTyped.Raise(keyPressEventArgs);
+        //        keyboard.KeyReleased.Raise(keyUpEventArgs);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        //oLog.WriteErrorLog(System.Reflection.MethodBase.GetCurrentMethod().Name, ex);
+        //    }
+        //}
     }
 }
